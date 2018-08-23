15073438ae5bf280a28bd1b1c1636ad1becfa9a5
zhangdy <dlzdy@163.com>
2018-08-23 23:35:48 +0800

    spring2.5.6兼容jdk1.8 可以启动web

--- parkingos_cloud_web/src/com/zld/struts/parkadmin/LiftRodAction.java
+++ parkingos_cloud_web/src/com/zld/struts/parkadmin/LiftRodAction.java
@@ -1,5 +1,23 @@
 package com.zld.struts.parkadmin;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
@@ -11,19 +29,12 @@ import com.zld.CustomDefind;
 import com.zld.impl.MongoClientFactory;
 import com.zld.impl.MongoDbUtils;
 import com.zld.service.DataBaseService;
-import com.zld.utils.*;
-import org.apache.log4j.Logger;
-import org.apache.struts.action.Action;
-import org.apache.struts.action.ActionForm;
-import org.apache.struts.action.ActionForward;
-import org.apache.struts.action.ActionMapping;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.*;
+import com.zld.utils.ExportExcelUtil;
+import com.zld.utils.JsonUtil;
+import com.zld.utils.RequestUtil;
+import com.zld.utils.SqlInfo;
+import com.zld.utils.StringUtils;
+import com.zld.utils.TimeTools;
 /**
  * 抬杆记录
  * @author Administrator
